f3256c9f881941dbfed5f568e88ca44b5252895f
Achintya151 <achintyakumartalukdar@gmail.com>
2022-10-26 19:58:08 +0600

    courses added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./Data/categories.json')
+app.use(cors());
+
+const categories = require('./Data/categories.json');
+const courses = require('./Data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Server running');
@@ -10,6 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/courses/', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
