83400f1aa5435e274e3632c3967cc175b686ec2e
Achintya151 <achintyakumartalukdar@gmail.com>
2022-10-26 17:11:10 +0600

    categories added

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./Data/categories.json')
+
 app.get('/', (req, res) => {
     res.send('Server running');
+})
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
 })
 
 app.listen(port, () => {
